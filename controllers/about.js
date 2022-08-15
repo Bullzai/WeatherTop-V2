237fc0533bd21eee808584217ab713deb776e530
bullzai <valskisv@gmail.com>
2022-08-15 15:12:36 +0100

    cleaned up and added about page

--- controllers/about.js
+++ controllers/about.js
@@ -4,7 +4,6 @@ const logger = require("../utils/logger");
 
 const about = {
   index(request, response) {
-    const myInt = 2;
     logger.info("about rendering");
     const viewData = {
       title: "About Weather Top V2",
